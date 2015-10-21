44f62d581df458b4baf3fdf0a3fde44c109ac9ff
N7Martin <mbrunaway@gmail.com>
2015-10-21 20:24:24 +0200

    Initial commit

---
+++ Zadanie12.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+// #include <errno.h>
+// #include <unistd.h>
+// #include <sys/types.h>
+// #include <sys/stat.h>
+// #include <fcntl.h>
+// #include <stdlib.h>
+
+int main (int argc, char* argv[]) {
+  
+  const char * pattern;
+
+  int i, patternSize;
+  int j = 0;
+  char user_input[250], *res;
+
+
+  if ( argc > 1 ) {
+    pattern = argv[1];
+    patternSize = strlen(pattern);
+  } else {    
+    return 0;
+  }  
+
+
+
+  res = fgets(user_input, 250, stdin);
+
+  if ( res == NULL ) {
+    //Komunikat o błędzie, bardzo niski nubie.
+    return 0;
+  }
+
+
+  //iteracja przez wypełnione elementy tablicy, zatrzymać się na pierwszej pustej kolumnie (duperele)
+  for( i=0; i < 250; i++ ) {
+
+    if ( user_input[i] == pattern[j] ) {
+      
+      j++;
+
+      if ( j == patternSize) {
+        if ( fputs( user_input, stdout ) == EOF ) {
+          //TODO Dodać komunikat o błędzie. Niskopoziomowo nubie.
+          return 0;  
+        }
+        
+      }
+
+    } else {
+      j = 0;
+    }
+
+  }
+
+  return 0;
+
+}
